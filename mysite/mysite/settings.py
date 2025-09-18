2ce74f5985374363ecd72a6b2e48bad6effc3dd2
DonatasNoreika <donoras@gmail.com>
2025-09-18 19:20:05 +0300

    pataisytas settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'autoservice/media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'autoservice/static')
 
 MEDIA_URL = '/media/'
 
